4504dafe9969b4b4006123e2082bf9e7079d8e5c
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-09-30 14:36:59 -0300

    up

--- src/id3/id3.java
+++ src/id3/id3.java
@@ -1,13 +1,24 @@
 package id3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class id3 {
 
 	public static void main(String[] args) {
 		//System.out.println("hello world ! ");
 		//objetivo é ler um arquivo .csv e "montar" uma arvore de decicao 
 		
-		String csvArquivo ="C:\\Users\\rsaluja\\CMS_Evaluation\\Drupal_12_08_27.csv";
-
+		String csvArquivo ="C:\\Users\\ghost\\dados.csv";
+		BufferedReader conteudoCSV = null;
+		String linha ="";
+		String csvSeparadorCampo = ";";
+		
+		try {
+			conteudoCSV = new BufferedReader(new FileReader(csvArquivo));
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
