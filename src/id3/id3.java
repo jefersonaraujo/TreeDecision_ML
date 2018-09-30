7c75990dbde7eb8a6ee6a3673d895a4652c7a312
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-09-30 14:31:51 -0300

    add

--- src/id3/id3.java
+++ src/id3/id3.java
@@ -3,7 +3,10 @@ package id3;
 public class id3 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world ! ");
+		//System.out.println("hello world ! ");
+		//objetivo é ler um arquivo .csv e "montar" uma arvore de decicao 
+		
+		String csvArquivo ="C:\\Users\\rsaluja\\CMS_Evaluation\\Drupal_12_08_27.csv";
 
 	}
 
