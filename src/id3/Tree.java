8aa4f6584bfa88bb95d9b3bd1708d6a84c987723
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-10-03 12:16:32 -0300

    lets go baby

--- src/id3/Tree.java
+++ src/id3/Tree.java
@@ -1,7 +1,14 @@
 package id3;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tree {
+	
+	private String raiz;
+	private String classe;
+	private String ramo;
+	private String rotulo;
 	
 
 }
