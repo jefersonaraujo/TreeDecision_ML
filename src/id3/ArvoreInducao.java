b765b3390dc299881e3b89bb62caa7e914e35973
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-10-20 17:05:01 -0300

    updte

--- src/id3/ArvoreInducao.java
+++ src/id3/ArvoreInducao.java
@@ -1,15 +1,19 @@
 package id3;
 
+import java.util.List;
+
 public class ArvoreInducao {
 
     String[] arvore_inducao;
     int id_no= 1;
     int id_pai= 0;
 
-
+    public List induzir_arvore() {
+    	return null;
+    }
 
 	public static void main(String[] args) {
-		
+		System.out.println("Here");
 
 	}
 
